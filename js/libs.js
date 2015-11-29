b4c9fe79656b59ffae8fa00f6666b6c27a9cf541
domenico <domenico@openpc.biz>
2015-11-29 20:42:38 +0100

    New modify

--- js/libs.js
+++ js/libs.js
@@ -1,6 +1,11 @@
 function check_firstLoad(){
+    var data = new Date();
     if(!localStorage.getItem("first")) {
             localStorage.setItem("first",true);
+            localStorage.setItem("data",data.toLocaleString());
+            localStorage.setItem("os",window.navigator.platform);
+            localStorage.setItem("lang",window.navigator.language);
+            //window.navigator.oscpu
         } else 
             {
                 localStorage.setItem("first",false);
@@ -8,5 +13,5 @@ function check_firstLoad(){
 };
 
 function func(){
-    document.writeln("cazzo");
+    document.writeln(window.navigator.language);
 };
