bc73276f8c623e69b4403966aecd5c4dc243a0d4
domenico <domenico@openpc.biz>
2015-11-29 19:46:08 +0100

    Include bootup.js

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include 'php/vars.php';
 
 include 'template/header.php';
         
-include 'template/home.php';
+include 'view/home.php';
 
 include 'template/footer.php';
--- template/header.php
+++ template/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Efor formazione e comunicazione</title>
+        <script <?php echo 'src='.'"'.$path_js.'bootup.js'.'"'?> type="text/javascript"></script>
     </head>
     <body>
 
